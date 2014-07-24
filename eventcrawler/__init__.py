099432f6dbfecee6da5cfb39a861b51b98f439d1
Dario Bertini <berdario@gmail.com>
2014-07-23 21:51:45 -0700

    Added asyncio crawler

--- eventcrawler/__init__.py
+++ eventcrawler/__init__.py
@@ -4,6 +4,7 @@ from functools import partial
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from asyncio import async, coroutine, get_event_loop, as_completed
 
 from lxml import html, etree
 
@@ -35,7 +36,7 @@ def filter_comments(nodes):
 
 def get_hierarchy(node):
     def get_numbered_tag(node):
-        if node.getparent():
+        if node.getparent() is not None:
             return node.tag + str(node.getparent().index(node))
         else:
             return node.tag
@@ -46,9 +47,11 @@ def get_hierarchy(node):
         parent = parent.getparent()
     return ">".join(get_numbered_tag(node) for node in reversed(parents))
 
+@coroutine
 def download(url):
     return html.parse(urlopen(url)).find('body')
 
+@coroutine
 def find_parent_page(urlstring):
     response = None
     url = urlparse(urlstring)
@@ -66,7 +69,7 @@ def find_parent_page(urlstring):
             else:
                 raise
         splitted_path = splitted_path[:-1]
-    return (parent_page, response)
+    return html.parse(response).find('body')
 
 def find_links(body):
     url = urlparse(body.base_url)
@@ -74,16 +77,43 @@ def find_links(body):
     return [n.attrib['href'] for n in body.xpath('.//a[@href]')
             if n.attrib['href'].startswith(base_url)]
 
+regexps = ['20\d\d', r'\(?\d{3}\)?\-?\s?\d{3}-?\d{4}']
+targets = base_targets + date_targets
+find_nodes = partial(_find_nodes, targets, regexps)
+
+def fingerprint_page(event_page):
+    page_nodes = filter_comments(find_nodes(event_page))
+    return {get_hierarchy(node) for node in page_nodes}
+
+@coroutine
+def crawl(links, visited, target_fingerprint):
+    results = []
+    while len(results) < 10 and len(links-visited) > 0:
+        pages = as_completed([async(download(url)) for url in links - visited])
+        visited.update(links)
+        links = set()
+        for future_page in pages:
+            page = yield from future_page
+            print('diff: ', target_fingerprint ^ fingerprint_page(page))
+            if target_fingerprint == fingerprint_page(page):
+
+                results.append(page.base_url)
+            links.update(find_links(page))
+    return results
+
+@coroutine
+def async_main():
+    event_url = 'http://www.workshopsf.org/?page_id=140&id=1328'
+    #'http://events.stanford.edu/events/353/35309/'
+    future_event_page = async(download(event_url))
+    future_event_parent = async(find_parent_page(event_url))
+    event_page = yield from future_event_page
+    event_parent = yield from future_event_parent
+    fingerprint = fingerprint_page(event_page)
+    links = set(find_links(event_parent))
+    result = yield from crawl(links, {event_url}, fingerprint)
+    return result
 
 def main():
-    s1 = 'http://events.stanford.edu/events/353/35309/'
-    s2 = 'http://www.workshopsf.org/?page_id=140&id=1328'
-    regexps = ['20\d\d', r'\(?\d{3}\)?\-?\s?\d{3}-?\d{4}']
-    targets = base_targets + date_targets
-    find_nodes = partial(_find_nodes, targets, regexps)
-    body = pq(s2)('body')[0]
-
-
-    n2 = filter_comments(find_nodes(body))
-    s2check = pq('http://www.workshopsf.org/?cat=6')('body')[0]
-    ncheck = filter_comments(find_nodes(s2check))
+    hits = list(get_event_loop().run_until_complete(async_main()))
+    print(*hits, sep='\n')
