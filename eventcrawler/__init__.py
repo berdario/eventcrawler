4df8d965dca85bef6cef9e2acfd0f5d800414343
Dario Bertini <berdario@gmail.com>
2014-07-24 02:51:20 -0700

    Cleanup: docstrings, comments, pep8

--- eventcrawler/__init__.py
+++ eventcrawler/__init__.py
@@ -15,7 +15,13 @@ date_targets = ['monday', 'tuesday', 'wednesday', 'thursday',
                 'march', 'april', 'may', 'june', 'july', 'august',
                 'september', 'october', 'november', 'december']
 
+regexps = ['20\d\d', r'\(?\d{3}\)?\-?\s?\d{3}-?\d{4}']
+targets = base_targets + date_targets
+
+
 def _find_nodes(targets, targets_regexps, body):
+    """Get all the interesting node in the page body
+    Interesting nodes are those which contain the target words"""
     regexps = [re.compile(rtext, re.IGNORECASE) for rtext in targets_regexps]
     queue = deque([body])
     candidates = []
@@ -26,13 +32,17 @@ def _find_nodes(targets, targets_regexps, body):
             queue.extend(node)
         text = (node.text or '').lower()
         if len(text) >= minlen and \
-        (any(t in text for t in targets) or \
-         any(r.match(text) for r in regexps)):
+           (any(t in text for t in targets) or
+            any(r.match(text) for r in regexps)):
                 candidates.append(node)
     return candidates
 
+find_nodes = partial(_find_nodes, targets, regexps)
+
+
 def filter_comments(nodes):
     return [node for node in nodes if not isinstance(node, etree.CommentBase)]
+
 
 def get_hierarchy(node):
     def get_numbered_tag(node):
@@ -47,6 +57,7 @@ def get_hierarchy(node):
         parent = parent.getparent()
     return ">".join(get_numbered_tag(node) for node in reversed(parents))
 
+
 def blocking_download(url):
     try:
         return html.parse(urlopen(url)).find('body')
@@ -59,8 +70,11 @@ def blocking_download(url):
 
 download = coroutine(blocking_download)
 
+
 @coroutine
 def find_parent_page(urlstring):
+    """Finds the first parent page that's not a 404
+    used as a starting point for the crawler"""
     response = None
     url = urlparse(urlstring)
     base_url = url.scheme + '://' + url.netloc
@@ -79,27 +93,28 @@ def find_parent_page(urlstring):
         splitted_path = splitted_path[:-1]
     return html.parse(response).find('body')
 
+
 def find_links(body):
+    """Get all the links (absolute or relative) pointing internally towards the same site"""
     url = urlparse(body.base_url)
     domain = url.scheme + '://' + url.netloc
+
     def add_base_url(url):
         if '://' not in url:
             return urljoin(body.base_url, url)
         else:
             return url
 
-    def filter_urls(url):
+    def select_url(url):
         return (url.startswith(domain) or '://' not in url) and \
             not url.startswith('javascript:') and not url.startswith('mailto:')
 
-    links = map(lambda u: add_base_url(u.attrib['href']), body.xpath('.//a[@href]'))
-    return set(filter(filter_urls, links))
+    links = [add_base_url(u.attrib['href']) for u in body.xpath('.//a[@href]')]
+    return set(filter(select_url, links))
 
-regexps = ['20\d\d', r'\(?\d{3}\)?\-?\s?\d{3}-?\d{4}']
-targets = base_targets + date_targets
-find_nodes = partial(_find_nodes, targets, regexps)
 
 def fingerprint_page(event_page):
+    """Returns the set of hierarchies for each interesting node"""
     page_nodes = filter_comments(find_nodes(event_page))
     return {get_hierarchy(node) for node in page_nodes}
 
@@ -122,20 +137,33 @@ def levenshtein(s1, s2):
         previous_row = current_row
     return previous_row[-1]
 
+
 def score_candidate(page, target_fingerprint):
+    """The more the interesting nodes are in the same place/structure in the page,
+    the more their hierarchy string representation should be similar.
+
+    In some lucky cases the nodes might be in the exact same positions, so that the
+    two sets/fingerprints will be the same, otherwise we'll try to check the distance for
+    every product/combinations of the hierarchy-strings not present in the other
+    fingerprint, but only if there're less than 37 such pairings"""
     fingerprint = fingerprint_page(page)
+    # if any of the 2 symmetric differences are empty, use the shortest string
+    # from the other set, this is to avoid penalizing too much pages that have
+    # fewer interesting nodes than the target page (for example)
     d1 = target_fingerprint - fingerprint or {min(fingerprint, key=len)}
     d2 = fingerprint - target_fingerprint or {min(target_fingerprint, key=len)}
     distance = None
-    #print(len(d1), len(d2))
     if len(d1)*len(d2) < 37:
-        distance = sum(min(levenshtein(x,y) for y in d2) for x in d1)
+        distance = sum(min(levenshtein(x, y) for y in d2) for x in d1)
         if 'events' in urlparse(page.base_url).path:
             distance /= 2
     print('distance:', distance, 'url:', page.base_url)
     return distance
 
+
 def sample_candidates(links, target_fingerprint):
+    """Get the average score for the first 5 candidates, so as to pick a
+    threshold for the crawling function"""
     samples = []
     while links:
         new_links = set()
@@ -146,15 +174,20 @@ def sample_candidates(links, target_fingerprint):
             if sample is not None:
                 samples.append(sample)
             if len(samples) == 5:
+                # clip to 10, in case the first few samples are too
+                # good compared to the following ones
                 return max(sum(samples)/5, 10)
         else:
             links = new_links
+
 
 @coroutine
 def crawl(links, visited, target_fingerprint):
     results = []
     sample_average = sample_candidates(links, target_fingerprint)
     print('average score:', sample_average)
+    # if we exhaust the links on the website, we will
+    # exit from the loop even with < 10 results
     while len(results) < 10 and len(links - visited) > 0:
         pages = as_completed([download(url) for url in links - visited])
         visited.update(links)
@@ -165,10 +198,11 @@ def crawl(links, visited, target_fingerprint):
                 continue
             score = score_candidate(page, target_fingerprint)
             if score is not None and score < sample_average:
-                #print('found! ', page.base_url)
+                # a running average might be better, but this will do for now
                 results.append(page.base_url)
             links.update(find_links(page))
     return results
+
 
 @coroutine
 def async_main():
@@ -185,6 +219,7 @@ def async_main():
     links = find_links(event_parent)
     result = yield from crawl(links, {event_url}, fingerprint)
     return result
+
 
 def main():
     hits = list(get_event_loop().run_until_complete(async_main()))
